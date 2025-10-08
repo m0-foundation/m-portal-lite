837bcab06c94c327d94cca34f92e00f461317ef8
0xIryna <iryna.hubernska@m0.xyz>
2025-10-07 17:41:50 -0700

    chore: deploy SpokePortal to BNB and BNB Testnet

--- script/config/HyperlaneConfig.sol
+++ script/config/HyperlaneConfig.sol
@@ -7,7 +7,7 @@ import { Chains } from "./Chains.sol";
 library HyperlaneConfig {
     /**
      * @notice Returns the address of Hyperlane Mailbox contract
-     * @dev    https://docs.hyperlane.xyz/docs/reference/addresses/mailbox-addresses
+     * @dev    https://docs.hyperlane.xyz/docs/reference/addresses/deployments/mailbox
      * @param  chainId_ EVM chain id.
      * @return mailbox_ The address of Hyperlane Mailbox contract.
      */
@@ -17,11 +17,13 @@ library HyperlaneConfig {
         if (chainId_ == Chains.HYPER_EVM) return 0x3a464f746D23Ab22155710f44dB16dcA53e0775E;
         if (chainId_ == Chains.PLUME) return 0x3a464f746D23Ab22155710f44dB16dcA53e0775E;
         if (chainId_ == Chains.LINEA) return 0x02d16BC51af6BfD153d67CA61754cF912E82C4d9;
+        if (chainId_ == Chains.BNB) return 0x2971b9Aec44bE4eb673DF1B88cDB57b96eefe8a4;
 
         // Testnet
         if (chainId_ == Chains.ETHEREUM_SEPOLIA) return 0xfFAEF09B3cd11D9b20d1a19bECca54EEC2884766;
         if (chainId_ == Chains.HYPER_EVM_TESTNET) return 0x589C201a07c26b4725A4A829d772f24423da480B;
         if (chainId_ == Chains.PLUME_TESTNET) return 0xDDcFEcF17586D08A5740B7D91735fcCE3dfe3eeD;
+        if (chainId_ == Chains.BNB_TESTNET) return 0xF9F6F5646F478d5ab4e20B0F910C92F1CCC9Cc6D;
 
         revert Chains.UnsupportedChain(chainId_);
     }
