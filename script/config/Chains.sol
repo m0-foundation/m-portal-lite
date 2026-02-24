3ed5410fcd0b2ce189e5c7a1c50aed01f1773bf4
Uyoyou Saint Uwuseba <uwusebauyoyou@gmail.com>
2026-02-24 17:17:06 +0100

    feat: add 0G chain (16661) deployment configuration (#55)
    * feat: add 0G chain (16661) deployment configuration
    
    - Add ZG chain constant (16661) to Chains.sol with Ethereum as hub
    - Add Hyperlane mailbox address for 0G to HyperlaneConfig.sol
    - Add Make targets: deploy-spoke-0g, deploy-spoke-wrapped_m-0g, configure-0g, transfer-0g, transfer-m-like-token-0g
    
    * chore: deploy spoke components to 0g
    
    * chore: deploy wrapped_m components to 0g
    
    * chore: configure hub chain on 0g

--- script/config/Chains.sol
+++ script/config/Chains.sol
@@ -16,6 +16,7 @@ library Chains {
     uint256 internal constant SONEIUM = 1868;
     uint256 internal constant PLASMA = 9745;
     uint256 internal constant CITREA = 4114;
+    uint256 internal constant ZG = 16661;
 
     // Testnet
     uint256 internal constant ETHEREUM_SEPOLIA = 11155111;
@@ -33,6 +34,7 @@ library Chains {
         if (spokeChainId_ == SONEIUM) return ETHEREUM;
         if (spokeChainId_ == PLASMA) return ETHEREUM;
         if (spokeChainId_ == CITREA) return ETHEREUM;
+        if (spokeChainId_ == ZG) return ETHEREUM;
 
         // Testnet
         if (spokeChainId_ == HYPER_EVM_TESTNET) return ETHEREUM_SEPOLIA;
